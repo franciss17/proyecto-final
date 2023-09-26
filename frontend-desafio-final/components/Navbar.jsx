217fd1fd51e580ad8724ca27a5deb80718567d77
Francisco Moya Sanhueza <110642666+franciscojavier17@users.noreply.github.com>
2023-09-26 16:09:43 -0300

    agregamos el menu hamburguesa en el navbar

--- frontend-desafio-final/components/Navbar.jsx
+++ frontend-desafio-final/components/Navbar.jsx
@@ -1,45 +1,42 @@
 import { Link } from "react-router-dom";
 import { useOperationsContext } from "../context/OperationsContext";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../context/AuthContext";
-import "../src/index.css";
 import logo from "../assets/img/logo3.svg";
 
 export default function Navbar() {
     const { total, FormatCoin } = useOperationsContext();
     const { user } = useContext(AuthContext);
-    const handleScrollToTop = () => {
-        window.scrollTo({
-            top: 0,
-            behavior: "smooth",
-        });
+    const [menuOpen, setMenuOpen] = useState(false);
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
     };
 
     return (
-        <nav className="navbar navbar-expand-lg fixed-top">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
             <div className="container">
-                <Link className="navbar-brand " to="/" onClick={handleScrollToTop}>
+                <Link className="navbar-brand" to="/">
                     <img src={logo} alt="Logo" width="60" height="55" />
                 </Link>
                 <button
-                    className="navbar-toggler"
+                    className={`navbar-toggler ${menuOpen ? "" : "collapsed"}`}
                     type="button"
                     data-bs-toggle="collapse"
                     data-bs-target="#navbarNav"
                     aria-controls="navbarNav"
-                    aria-expanded="false"
+                    aria-expanded={menuOpen ? "true" : "false"}
                     aria-label="Toggle navigation"
+                    onClick={toggleMenu}
                 >
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
+                <div className={`collapse navbar-collapse ${menuOpen ? "show" : ""}`} id="navbarNav">
                     <ul className="navbar-nav ms-auto mb-2 mb-lg-0 fs-5">
                         <li className="nav-item">
                             <Link className="nav-link" to="/">
-                                Home
+                                Inicio
                             </Link>
                         </li>
-                        
                         <li className="nav-item">
                             <Link className="nav-link" to="/Login">
                                 Iniciar sesión
@@ -50,7 +47,7 @@ export default function Navbar() {
                                 Registrate
                             </Link>
                         </li>
-                        {user && ( // Mostrar el enlace solo si el usuario ha iniciado sesión
+                        {user && (
                             <li className="nav-item">
                                 <Link className="nav-link" to="/Payment">
                                     Página de Pago
