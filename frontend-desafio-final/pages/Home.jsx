bba7887b99c99769be98a21eaf8c27a8678c0ac4
Francisco Moya Sanhueza <110642666+franciscojavier17@users.noreply.github.com>
2023-09-26 18:29:48 -0300

    arreglo de carrusel

--- frontend-desafio-final/pages/Home.jsx
+++ frontend-desafio-final/pages/Home.jsx
@@ -10,37 +10,39 @@ export default function Products() {
         return (
             <div className="container p-5 mt-5">
                 <div>
-                    <h3 className="text-center">{error}</h3>
+                    <h3 className="text-center error-message">{error}</h3>
                 </div>
             </div>
         );
     }
 
     return (
-        <>
+        <div className="products-container">
             <Header />
-            <div className="container p-5 mt-5">
+            <div className="carousel">
                 <Carousel>
                     {pizzas.map((item) => (
                         <Carousel.Item key={item.id}>
                             <img
-                                className="d-block w-100"
+                                className="d-block w-100 pizza-image"
                                 src={item.img}
                                 alt={item.name}
                             />
                             <Carousel.Caption>
-                                <h3>{item.name}</h3>
-                                <p>{item.desc}</p>
+                                <h3 className="pizza-name">{item.name}</h3>
+                                <p className="pizza-description">{item.desc}</p>
                             </Carousel.Caption>
                         </Carousel.Item>
                     ))}
                 </Carousel>
+            </div>
+            <div className="container p-5 mt-5">
                 <div className="row">
                     {pizzas.map((item) => (
                         <CardPizza pizza={item} key={item.id} />
                     ))}
                 </div>
             </div>
-        </>
+        </div>
     );
 }
