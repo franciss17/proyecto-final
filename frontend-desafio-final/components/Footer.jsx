d95beb3ac0eca1d3d67ae12e4cef3e1a01703f38
Francisco Moya Sanhueza <110642666+franciscojavier17@users.noreply.github.com>
2023-10-11 20:18:46 -0300

    correcting errors in the code and also editing the footer

--- frontend-desafio-final/components/Footer.jsx
+++ frontend-desafio-final/components/Footer.jsx
@@ -9,6 +9,15 @@ const Footer = () => {
                 <div className="footer-top">
                     <div className="footer-location">
                         <h3>Ubicación</h3>
+                        <iframe
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d65280125.95684839!2d-74.48435958635211!3d2.991130998912869!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12d591a6c44e88cd%3A0x32eca9b1d554fc03!2sTorre%20de%20Pisa!5e0!3m2!1ses-419!2scl!4v1697064381187!5m2!1ses-419!2scl"
+                            width="200"
+                            height="200"
+                            style={{ border: "0" }}
+                            allowfullscreen=""
+                            loading="lazy"
+                            referrerpolicy="no-referrer-when-downgrade">
+                        </iframe>
                         <p>Santiago, Chile</p>
                     </div>
                     <div className="footer-links">
@@ -18,7 +27,7 @@ const Footer = () => {
                                 <a href="#">Inicio</a>
                             </li>
                             <li>
-                                <a href="#">Contacto</a>
+                                <a href="mailto:contacto.chef@gmail.com">Contacto</a>
                             </li>
                         </ul>
                     </div>
